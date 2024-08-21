fa77460dad843c2c3f548816cea38d2550fbeba3
Alexander Cairns <alexandercairns@discoverygarden.ca>
2024-08-21 20:54:31 -0300

    of not in

--- dist/index.js
+++ dist/index.js
@@ -42896,7 +42896,7 @@ async function run() {
     (file) => file.filename.endsWith('values.yaml')
   )
 
-  for (file in changed_values_files){
+  for (file of changed_values_files){
     core.info(file)
     const base_file = await octokit.rest.repos.getContent({
       ...context.repo,
